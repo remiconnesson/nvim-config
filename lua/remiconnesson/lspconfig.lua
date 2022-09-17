e5f10cb87b83049e24277b730d66fec07f86c6df
Rémi Connesson <remiconnesson@gmail.com>
2022-09-17 19:25:37 +0200

    Toggle diagnostics on and off

--- lua/remiconnesson/lspconfig.lua
+++ lua/remiconnesson/lspconfig.lua
@@ -162,3 +162,18 @@ lspconfig_configs.volar_html = {
 lspconfig.volar_html.setup{
     on_attach = on_attach
 } 
+
+
+-- toggle diagnostics
+
+vim.g.diagnostics_visible = true
+function _G.toggle_diagnostics()
+  if vim.g.diagnostics_visible then
+    vim.g.diagnostics_visible = false
+    vim.diagnostic.disable()
+  else
+    vim.g.diagnostics_visible = true
+    vim.diagnostic.enable()
+  end
+end
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>tt', ':call v:lua.toggle_diagnostics()<CR>', {silent=true, noremap=true})
